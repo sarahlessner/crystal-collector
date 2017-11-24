bd264bc0d626a8d1fce9f6a22742cdbafc950247
Sarah Lessner <sarahlessner@gmail.com>
2017-11-23 23:33:31 -0800

    cleaned up code for old project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,26 +9,23 @@ var userTotal = 0;
 
 initRound();
 
-
 function initRound() {
 	//generate and display random number and user total
 	guessNumber = Math.round(Math.random() * 101) + 19;
-	$("#number-to-guess").text(guessNumber);	
+	$("#number-to-guess").text(guessNumber);
 	userTotal = 0;
 	$("#total-score").text(userTotal);
 	//generate crystals with random number user will click to add to total score
 	for (i = 0; i < crystalImgArray.length; i++) {
-	var crystalImg = $("<img>");
-	crystalImg.addClass("crystal-image");
-	crystalImg.attr("src", crystalImgArray[i]);
-	crystalImg.attr("data-crystalvalue", Math.round(Math.random()* 11) + 1);
-	$("#crystals").append(crystalImg);
-
+		var crystalImg = $("<img>");
+		crystalImg.addClass("crystal-image");
+		crystalImg.attr("src", crystalImgArray[i]);
+		crystalImg.attr("data-crystalvalue", Math.round(Math.random()* 11) + 1);
+		$("#crystals").append(crystalImg);
 	};
 
-	$(".crystal-image").on("click", clickCrystal);	
+	$(".crystal-image").on("click", clickCrystal);
 };
-
 
 function clickCrystal() {
 	//this is here for the first click of a new game to clear win/loss message
@@ -36,32 +33,23 @@ function clickCrystal() {
 	//take the rando number for the crystals and create variable to store as integer
 	//increment user total by the value of the crystal clicked
 	var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    userTotal += crystalValue;
-    $("#total-score").text(userTotal);
-
+  crystalValue = parseInt(crystalValue);
+  userTotal += crystalValue;
+  $("#total-score").text(userTotal);
     //win condition + initialize a new round
-    if (userTotal === guessNumber) {
-    	$("#win-loss-stat").text("You Win!");
-    	wins ++;
-    	$("#wins").text(wins);
-    	$("#crystals").empty();
-    	initRound();
-    }
-    //loss condition + initialize a new round
-    else if (userTotal >= guessNumber) {
-    	$("#win-loss-stat").text("You Lose!");
-    	losses++;
-    	$("#losses").text(losses);
-    	$("#crystals").empty();
-    	initRound();
-    }
-
+  if (userTotal === guessNumber) {
+  	$("#win-loss-stat").text("You Win!");
+  	wins ++;
+  	$("#wins").text(wins);
+  	$("#crystals").empty();
+  	initRound();
+  }
+  //loss condition + initialize a new round
+  else if (userTotal >= guessNumber) {
+  	$("#win-loss-stat").text("You Lose!");
+  	losses++;
+  	$("#losses").text(losses);
+  	$("#crystals").empty();
+  	initRound();
+  }
 };
-
-
-
-
-
-
-
